a27bcd2fe8f31d348aef4b770aa77f333b80ed38
Damian Pagowski <d.pagowski@gmail.com>
2020-04-08 19:26:39 +0700

    added unit tests. fixed webpack config for dev

--- test/formHandler.test.js
+++ test/formHandler.test.js
@@ -3,6 +3,5 @@ const formHandler = require('../src/client/js/formHandler.js');
 
 test('form handler should have function handleSubmit', () => {
   expect(typeof formHandler.handleSubmit).toBe("function");
-  // typeof formHandler.handleSubmit === "function") { 
 });
 
